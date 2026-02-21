6a80d45cbbc013ab2f39952b0a302ad22ec2c6b3
vipha <dimvipha96@gmail.com>
2026-02-21 09:37:29 +0700

    folder structure

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
+    
 @app.get("/")
 async def root():
     return {"message": "Hello World!!"}
